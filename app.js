c8bfd68891633c5f656f04628059a46bec83c381
Facundo Robles <robles.facu@gmail.com>
2021-09-02 14:07:43 -0300

    CRUD API finished, Adding basic front end

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const mysql = require('mysql');
 const morgan = require('morgan');
-// const methodOverride = require('method-override');
-
+const methodOverride = require('method-override');
 
 const myWalletRoutes = require('./routes/myWallet');
-const userRoutes = require('./routes/users')
+const userRoutes = require('./routes/users');
 
 //EXPRESS INIT
 const app = express();
@@ -15,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 
 //METHOD OVERRIDE
-// app.use(methodOverride('_method'));
+app.use(methodOverride('_method'));
 
 //EJS TEMPLATES
 app.engine('ejs', ejsMate);
@@ -25,7 +24,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 //MIDDLEWARES
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 //GLOBAL VARIABLES
 
@@ -33,39 +32,14 @@ app.use(morgan('dev'))
 //MY WALLET ROUTE
 app.use('/mywallet', myWalletRoutes);
 //USER ROUTES
-app.use('/',userRoutes);
+app.use('/', userRoutes);
 //HOME ROUTE
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-
-//DB CREATE
-
-// app.get('/createdb', (req,res) =>{
-    //     let sql ='CREATE DATABASE virtualWallet';
-    //     db.query(sql, (err, result)=>{
-        //         if(err) throw err;
-        //         res.send('database created');
-        //         console.log(result);
-        //     })
-        // })
-        //MY SQL CONFIG
-        // const db = mysql.createConnection({
-        //     host: 'localhost',
-        //     user: 'root',
-        //     password: '123456',
-        //     database: 'virtual_wallet'
-        // });
-        
-        // //MY SQL CONNECT
-        // db.connect((err)=>{
-        //     if(err) throw err;
-        //     console.log('MySql Connected...')
-        // });
-        
-        //STARTING SERVER
-        const port = process.env.PORT || 3000;
+//STARTING SERVER
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`serving on port ${port}`);
 });
--- routes/myWallet.js
+++ routes/myWallet.js
@@ -1,16 +1,51 @@
-const express= require('express');
+const express = require('express');
 const router = express.Router();
 const pool = require('../database');
+const catchAsync = require('../utils/catchAsync');
 
-//INDEX
-router.get('/', (req, res) => {
-    res.render('mywallet/index');
+const categories = ['Income', 'Outcome'];
+//INDEX CREATE AND READ OPERATIONS
+router.route('/')
+    .get(catchAsync(async (req, res) => {
+        const operations = await pool.query('SELECT * FROM operations');
+        res.render('mywallet/index', { operations });
+    }))
+    .post(catchAsync(async (req, res) => {
+        const { operation } = req.body;
+        console.log(operation);
+        await pool.query('INSERT INTO operations set ?', [operation]);
+        res.redirect('/mywallet/index');
+    }));
+
+//NEW FORM
+
+router.get('/new', (req, res) => {
+    res.render('mywallet/new');
 });
 
-//NEW
-router.get('/new', (req,res)=>{
-    res.render('mywallet/new');
-})
-
+//UPDATE AND DELETE OPERATION
+router.route('/:id')
+    .get(catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const rawData = await pool.query(
+            `SELECT * FROM operations WHERE id =${id}`
+        );
+        const operation = rawData[0];
+        console.log(operation);
+        res.render('mywallet/edit', { operation, categories });
+    }))
+    .put(catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const { title, value, category, body } = req.body.operation;
+        await pool.query(
+            `UPDATE operations SET title='${title}', value=${value}, category='${category}', body='${body}' WHERE id =${id}`
+        );
+        res.redirect('/mywallet');
+    }))
+    .delete(catchAsync(async (req, res) => {
+        const { id } = req.params;
+        await pool.query(`DELETE FROM operations WHERE id =${id}`)
+        res.redirect('/mywallet');
+    }));
 
 module.exports = router;
